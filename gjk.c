f79b8db86a85763e5bd9ddfbeb75a20faa10b27b
Igor Kroitor <igor.kroitor@gmail.com>
2016-11-29 01:32:06 +0300

    Update gjk.c

--- gjk.c
+++ gjk.c
@@ -82,7 +82,7 @@ vec2 support (const vec2 * vertices1, size_t count1,
     // get furthest point of second body along the opposite direction
     size_t j = indexOfFurthestPoint (vertices2, count2, negate (d));
 
-    // to see if bodies overlap along all directions in Minkowski space
+    // see if bodies overlap along all directions in Minkowski space
     return subtract (vertices1[i], vertices2[j]);
 }
 
